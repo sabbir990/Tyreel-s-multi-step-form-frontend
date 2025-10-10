30b56587f8e686aad83024d7353f9ab26e0beae6
Romicha Parvin <romichaparvin35@gmail.com>
2025-10-10 17:07:50 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const nextBtnSlide1 = document.getElementById("nextSlide1");
+const step2 = document.getElementById("step2");
+const step1 = document.getElementById("step1")
+
+// step2.classList.add("hidden")
+
+// nextBtnSlide1.addEventListener("click", () => {
+//     step1.classList.add("hidden")
+//     step2.classList.remove("hidden")
+// })
